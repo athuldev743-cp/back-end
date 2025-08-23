f28c472da054670bfb1891f06b969d02448dc5f2
athul dev <athuldev743@gmail.com>
2025-08-23 14:00:46 +0530

    Update database connection logic

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ MONGO_URL = os.getenv("MONGO_URL")
 if not MONGO_URL:
     raise RuntimeError("MONGO_URL environment variable is missing")
 
-client = MongoClient(MONGO_URL)
+client = MongoClient(MONGO_URL, tls=True, tlsAllowInvalidCertificates=False)
 db = client["realestate"]
 
 # Optional: Test connection
