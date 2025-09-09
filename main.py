a65b75d567699b06dc32d46e551ed36de872e72f
athul dev <athuldev743@gmail.com>
2025-09-09 08:17:49 +0530

    bhbcjd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 # -------------------- CORS --------------------
 origins = [
-    "https://real-estate-front-6o4p5btyx-athul-devs-projects.vercel.app",  # your production frontend
+    "https://real-estate-front-two.vercel.app",  
     "http://localhost:3000",           # local testing
 ]
 
